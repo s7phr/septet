670c0d84593a20f45b43ec12625a76733b2270c0
= <sipher@creepy.sh>
2024-02-11 15:59:28 -0600

    Fixed routes not dispatching properly

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from app import App
 from routes import *
 
 Client = App()
+
 Client.app.register_blueprint(police, url_prefix="/police")
 Client.app.register_blueprint(checkpoint, url_prefix="/checkpoint")
+Client.app.register_blueprint(invoice, url_prefix="/invoice")
 
 Client.run()
